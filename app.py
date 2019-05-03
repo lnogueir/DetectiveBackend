301fa11c92d1cac6257af6d9b159f36d2ca779c8
Lucas Nogueira <lnogueir@uwaterloo.ca>
2019-05-03 18:12:30 -0400

    game running successfully on flask

--- app.py
+++ app.py
@@ -1,44 +1,27 @@
-from flask import Flask
-from flask_restful import Api
-from flask_jwt import JWT
-from security import authentication, identity
-from resources.users import UserRegister,User
-from resources.tags import Tag,Tags
-from resources.topic import Topic,TopicList
-from resources.graph import Graph
-from resources.emailer import Email
-from resources.friends import Friend
 
+
+from flask import Flask,jsonify,request,render_template
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = '123456'
 
-api = Api(app)
-@app.before_first_request
-def create_tables():
-    db.create_all()
+## FIRST LECTURE ##
 
+# @app.route('/')
+# def home():
+#     print('Something...') # Se eu printo aqui, sempre que eu der um GET
+#     return '{"jobs":[ 1,2,3,4]}'# eu vou printar Something...
 
-
-jwt = JWT(app,authentication,identity)
+# // #       # // #
 
 
-api.add_resource(UserRegister,'/register')
-api.add_resource(User,'/user/<string:username>')
-api.add_resource(Topic,'/topic/<string:username>/<string:topic_name>')
-api.add_resource(TopicList,'/topics/<string:topic_name>')
-api.add_resource(Tag,'/tag/<int:topic_id>/<string:keyword>')
-api.add_resource(Tags,'/tags/<int:topic_id>')
-api.add_resource(Graph,'/graph')
-api.add_resource(Email,'/email/<string:username>')
-api.add_resource(Friend,'/friend/<string:username>')
+@app.route('/')
+def homepage():
+    return render_template("index.html")
 
 
+@app.route('/game')
+def game():
+    return render_template("game.html")
 
 
-if __name__ == '__main__':
-	from db import db
-	db.init_app(app)
-	app.run(port=5000)
+app.run(port=5000)
